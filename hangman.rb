f979ca576e0d0079dff5e8b2f82a15a1946c985b
penach0 <josemanuel.penacho@gmail.com>
2022-09-17 18:11:49 +0100

    Select Word From Dictionary
    - Split 'words' string into an array to enable the use of
    enumerable methods
    - Create pick_word method which takes an array of words and
    selects those that are between 5 and 12 characters long
    Afterwards it returns a random sample from the words that meet
    the constraints

--- hangman.rb
+++ hangman.rb
@@ -1,4 +1,8 @@
 dictionary = File.open('google-10000-english-no-swears.txt', 'r')
-words = dictionary.read
+words = dictionary.read.split("\n")
 
-puts words
+def pick_word(array)
+  array.select { |word| word.length.between?(5, 12) }.sample
+end
+
+pp pick_word(words)
