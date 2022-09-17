5e4c5378d10349ff0a532c5b04baa9ccd3f53129
penach0 <josemanuel.penacho@gmail.com>
2022-09-17 18:26:22 +0100

    Create Secret Word Class
    - Create class to initialize the secret word
    - Move all dictionary related operations to Dictionary module
    so it can be used by the SecretWord class

--- hangman.rb
+++ hangman.rb
@@ -1,8 +1,22 @@
-dictionary = File.open('google-10000-english-no-swears.txt', 'r')
-words = dictionary.read.split("\n")
+# Stores the dictionary and its related operations
+module Dictionary
+  dictionary = File.open('google-10000-english-no-swears.txt', 'r')
+  WORDS = dictionary.read.split("\n")
 
-def pick_word(array)
-  array.select { |word| word.length.between?(5, 12) }.sample
+  def pick_word(array)
+    array.select { |word| word.length.between?(5, 12) }.sample
+  end
 end
 
-pp pick_word(words)
+# Creates instance of the secret word
+class SecretWord
+  include Dictionary
+  attr_reader :secret_word
+
+  def initialize
+    @secret_word = pick_word(WORDS)
+  end
+end
+
+new_word = SecretWord.new
+puts new_word.secret_word
