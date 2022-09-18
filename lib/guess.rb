bf543e186b89acdc6753373159a65ca0aeb0a4f2
penach0 <josemanuel.penacho@gmail.com>
2022-09-18 10:59:29 +0100

    Create Guess Class
    - New guess class creates instances of letter guesses
    - player_input method on Guess class prompts user for a letter
    and restricts input to a letter through a warning message
    - The letter is downcased before assignment for case insensitive
    checks

---
+++ lib/guess.rb
@@ -0,0 +1,19 @@
+# Creates instances of new guesses
+class Guess
+  attr_reader :guess
+
+  def initialize
+    @guess = player_input.downcase
+  end
+
+  def player_input
+    letter = ''
+    loop do
+      letter = gets.chomp.strip
+      break if letter.size == 1 && letter.match?(/[a-zA-Z]/)
+
+      puts "That's not a letter! Please try again:"
+    end
+    letter
+  end
+end
