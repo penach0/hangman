9cc3504f34f75c0af2363384d3a0a811d9ac11b8
penach0 <josemanuel.penacho@gmail.com>
2022-09-18 12:11:48 +0100

    Create functionality regarding wrong guesses
    - Create instance variable array '@wrong_guesses' to store all
    the letters guessed wrong by the user
    - add_wrong_guesses passes the guess to the array if wrong
    - display_wrong_guesses outputs wrong letters to the user + number
    of tries left
    - Change the while loop on Game#play to run the game until user
    guesses wrong 8 times

--- lib/game.rb
+++ lib/game.rb
@@ -3,22 +3,32 @@ require_relative 'guess'
 
 # Contains logic of the gameplay
 class Game
-  attr_reader :secret_word
+  attr_reader :secret_word, :wrong_guesses
 
   MAX_WRONG_TRIES = 8
 
   def initialize
     @secret_word = SecretWord.new
+    @wrong_guesses = []
     puts secret_word.secret_word
   end
 
   def play
-    i = 0
-    while i < MAX_WRONG_TRIES
+    while wrong_guesses.size < MAX_WRONG_TRIES
       puts secret_word.display_word_state
-      guess = Guess.new
-      secret_word.update_word_state(secret_word.check_guess(guess.guess), guess.guess)
-      i += 1
+      guess = Guess.new.guess
+      secret_word.update_word_state(secret_word.check_guess(guess), guess)
+      add_wrong_guesses(guess)
+      display_wrong_guesses
     end
+  end
+
+  def add_wrong_guesses(guess)
+    wrong_guesses << guess unless secret_word.letters.include?(guess)
+  end
+
+  def display_wrong_guesses
+    puts "Wrong Guesses: #{wrong_guesses.join(' ').upcase}"
+    puts "Number of tries left: #{MAX_WRONG_TRIES - wrong_guesses.size}"
   end
 end
