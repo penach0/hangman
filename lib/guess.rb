caac05b8de2040c4979feaed343783a2162dd058
penach0 <josemanuel.penacho@gmail.com>
2022-09-18 16:11:33 +0100

    Prevent user from picking the same letter
    - Add Guess#already_picked? to check if a letter was already guessed,
    preventing the player from doing so while warning them

--- lib/guess.rb
+++ lib/guess.rb
@@ -2,18 +2,30 @@
 class Guess
   attr_reader :guess
 
+  @@all_guesses = []
+
   def initialize
     @guess = player_input
+    @@all_guesses << guess
   end
 
   def player_input
     letter = ''
     loop do
       letter = gets.chomp.strip
+      next if already_picked?(letter)
+
       break if letter.size == 1 && letter.match?(/[a-zA-Z]/)
 
       puts "That's not a letter! Please try again:"
     end
     letter.downcase
+  end
+
+  def already_picked?(guess)
+    if @@all_guesses.include?(guess)
+      puts 'You already picked that one, I\'ll give you one more try:'
+      true
+    end
   end
 end
