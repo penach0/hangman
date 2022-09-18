6819642377640b254af531c8038b8496a7d03958
penach0 <josemanuel.penacho@gmail.com>
2022-09-18 11:29:31 +0100

    Move logic of game from main.rb to Game class
    - Logic that was previously in main as a way of testing things was
    moved to a 'play' method inside the Game class

--- lib/game.rb
+++ lib/game.rb
@@ -1,7 +1,24 @@
 require_relative 'secret_word'
 require_relative 'guess'
 
+# Contains logic of the gameplay
 class Game
-  MAX_TRIES = 8
+  attr_reader :secret_word
 
+  MAX_WRONG_TRIES = 8
+
+  def initialize
+    @secret_word = SecretWord.new
+    puts secret_word.secret_word
+  end
+
+  def play
+    i = 0
+    while i < MAX_WRONG_TRIES
+      puts secret_word.display_word_state
+      guess = Guess.new
+      secret_word.update_word_state(secret_word.check_guess(guess.guess), guess.guess)
+      i += 1
+    end
+  end
 end
--- lib/main.rb
+++ lib/main.rb
@@ -1,13 +1,5 @@
-require_relative 'secret_word'
+require_relative 'game'
 
-new_word = SecretWord.new
-puts new_word.secret_word
-puts new_word.display_word_state
 
-i = 0
-while i < 10
-  guess = gets.chomp
-  new_word.update_word_state(new_word.check_guess(guess), guess)
-  puts new_word.display_word_state
-  i += 1
-end
+Game.new.play
+
