b3477fd2d0ef5c4318a018b34748b552f27ac395
penach0 <josemanuel.penacho@gmail.com>
2022-09-18 09:27:29 +0100

    Create check_guess method
    - Create @letters instance variables to work with the secret word
    as an array, seems more useful for substitutions later
    - check_guess method finds the positions of the matching letters, if
    any, and returns them

--- hangman.rb
+++ hangman.rb
@@ -11,17 +11,27 @@ end
 # Creates instance of the secret word
 class SecretWord
   include Dictionary
-  attr_reader :secret_word
+  attr_reader :secret_word, :letters
 
   def initialize
     @secret_word = pick_word
+    @letters = secret_word.split('')
   end
 
   def blank_lines
     '_ ' * secret_word.length
+  end
+
+  def check_guess(guess)
+    correct_positions = []
+    letters.each_with_index do |letter, index|
+      correct_positions << index if letter == guess
+    end
+    puts correct_positions
   end
 end
 
 new_word = SecretWord.new
 puts new_word.secret_word
 puts new_word.blank_lines
+new_word.check_guess('a')
