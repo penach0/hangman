58d699ea662b7a40e88b015fd968fa2ea034def1
penach0 <josemanuel.penacho@gmail.com>
2022-09-18 09:56:11 +0100

    Create update_word_state method
    - This method substitutes the letters in the right positions

--- hangman.rb
+++ hangman.rb
@@ -23,16 +23,25 @@ class SecretWord
     blank_lines.join(' ')
   end
 
+  def update_word_state(correct_positions, guess)
+    blank_lines.each_index do |index|
+      blank_lines[index] = guess if correct_positions.include?(index)
+    end
+    blank_lines
+  end
+
   def check_guess(guess)
     correct_positions = []
     letters.each_with_index do |letter, index|
       correct_positions << index if letter == guess
     end
-    puts correct_positions
+    correct_positions
   end
 end
 
 new_word = SecretWord.new
 puts new_word.secret_word
 puts new_word.display_word_state
-new_word.check_guess('a')
+
+new_word.update_word_state(new_word.check_guess('a'), 'a')
+puts new_word.display_word_state
