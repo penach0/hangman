adf2810809e83fb1b512e60b1db2756a3859c300
penach0 <josemanuel.penacho@gmail.com>
2022-09-21 14:19:57 +0100

    Polish game saving functionality
    - Loaded game now automatically saves to the same file it was
    loaded from

--- lib/game.rb
+++ lib/game.rb
@@ -10,27 +10,29 @@ class Game
   include Display
   include UserInput
   extend Saves
-  attr_reader :secret_word, :wrong_guesses, :all_guesses
+  attr_reader :secret_word, :wrong_guesses, :all_guesses, :file_name
 
   MAX_WRONG_TRIES = 8
 
-  def initialize(secret_word = nil, wrong_guesses = [], all_guesses = [])
+  def initialize(secret_word = nil, wrong_guesses = [], all_guesses = [], file_name = nil)
     @secret_word = secret_word || SecretWord.new
     @wrong_guesses = wrong_guesses || []
     @all_guesses = all_guesses || []
+    @file_name = file_name
     display_all
   end
 
   def self.load
-    saved_game = choose_saved_game(saved_games)
-    file = File.open("../saves/#{saved_game}.yaml", 'r')
+    file_name = choose_saved_game(saved_games)
+    file = File.open("../saves/#{file_name}.yaml", 'r')
     saved_game = YAML.safe_load(file, permitted_classes: [Game, SecretWord])
     file.close
 
     new(
       saved_game.secret_word,
       saved_game.wrong_guesses,
-      saved_game.all_guesses
+      saved_game.all_guesses,
+      file_name
     )
   end
 
@@ -75,7 +77,7 @@ class Game
   end
 
   def serialize
-    save_name = ask_save_name
+    save_name = file_name || ask_save_name
     file = File.open("../saves/#{save_name}.yaml", 'w')
     YAML.dump(self, file)
     file.close
