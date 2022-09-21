3bcec7a893ef21ab532a0d915dbdfb1608c269cf
penach0 <josemanuel.penacho@gmail.com>
2022-09-21 17:02:37 +0100

    Refactor Saves module
    - Saves module can now provide both instance and class methods to
    the Game class

--- lib/saves.rb
+++ lib/saves.rb
@@ -1,20 +1,27 @@
 # Contains methods related to the saving games functionality
 module Saves
-  def saved_games
-    Dir.children('../saves').map { |file| file.split('.')[0] }
+  def self.included(klass)
+    klass.extend(ClassMethods)
   end
 
-  def choose_saved_game(saved_games)
-    puts 'Pick a saved game (enter the corresponding number):'
-    saved_games.each.with_index(1) { |file, index| puts "#{index}. #{file}" }
-
-    choice = ''
-    loop do
-      choice = gets.chomp.to_i
-      break if [*1..saved_games.size].include?(choice)
-
-      puts 'Not a valid option'
+  # Save related class methods for Game
+  module ClassMethods
+    def saved_games
+      Dir.children('../saves').map { |file| file.split('.')[0] }
     end
-    saved_games[choice - 1]
+
+    def choose_saved_game(saved_games)
+      puts 'Pick a saved game (enter the corresponding number):'
+      saved_games.each.with_index(1) { |file, index| puts "#{index}. #{file}" }
+
+      choice = ''
+      loop do
+        choice = gets.chomp.to_i
+        break if [*1..saved_games.size].include?(choice)
+
+        puts 'Not a valid option'
+      end
+      saved_games[choice - 1]
+    end
   end
 end
